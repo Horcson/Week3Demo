72e3b60d87d48861087b0e854d029bf102b50118
Horcson <ow184@student.aru.ac.uk>
2022-10-06 11:29:58 +0100

    Update program so that days of the week are output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Sandra!");
-Console.WriteLine("Goodbye Sandra");
+int day;
+
+Console.WriteLine("Please enter a number between 1 and 7");
+try
+{
+
+    day = Convert.ToInt32(Console.ReadLine());
+
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+
+        default:
+            Console.WriteLine("1-7...");
+            break;
+
+    }
+}
+catch
+{
+    Console.WriteLine("Incorrect input");
+}
+
 Console.ReadKey();
